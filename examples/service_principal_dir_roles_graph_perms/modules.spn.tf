1df301a37d6114bb872891770839087d2c92f151
Curtis Slone <curtisdslone@gmail.com>
2023-05-16 12:43:02 +0200

    Updated README

--- examples/service_principal_dir_roles_graph_perms/modules.spn.tf
+++ examples/service_principal_dir_roles_graph_perms/modules.spn.tf
@@ -2,11 +2,11 @@
 # DSC Creation
 #
 
-module "dsc_spn" {
+module "mod_service_principal" {
     source  = "../terraform-azuread-overlays-service-principal/"
 
-    service_principal_name = "dsc_spn"
-    service_principal_description = "Service Principal that manages the M365DSC"
+    service_principal_name = "mod_service_principal"
+    service_principal_description = "Service Principal Example"
 
     enable_service_principal_certificate = false
     service_principal_password_rotation_in_years = 1
@@ -33,7 +33,7 @@ module "dsc_spn" {
 
   # Adding Directory Roles
   service_principal_directory_roles = [
-    "fdd7a751-b60b-444a-984c-02652fe8fa1c",
-    "4d6ac14f-3453-41d0-bef9-a3e0c569773a"
+    "fdd7a751-b60b-444a-984c-02652fe8fa1c", // Groups Administrator
+    "4d6ac14f-3453-41d0-bef9-a3e0c569773a"  // License Administrator
   ]
 }
